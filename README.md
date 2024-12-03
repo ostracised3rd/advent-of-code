d900ee6375ea30a904935af210ddd14a0353ef7f
soheil eivazy <eivazy.soheil@gmail.com>
2024-12-04 03:15:08 +0330

    Add readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of code
+
+## to run each day and part
+
+```sh
+cargo run -- -y [year: 2020|2021|2023|2024] -d [day: 1..25] -p [part: 1|2]
+```
--- src/aoc2024/d02.rs
+++ src/aoc2024/d02.rs
@@ -3,18 +3,14 @@ pub struct AoC;
 impl advent_of_rust::Day for AoC {
     fn p1(&self, data: String) {
         let ll = data_parser(data);
-        let count = safe_count(ll);
+        let count = safe_count(ll, is_safe);
         dbg!(count);
     }
     
-    fn p2(&self, _data: String) {
-        // let (l1, l2) = data_parser(data);
-        // let m2 = into_map(l2);
-        // let res = similarity(l1, m2);
-
-        
-        
-        // dbg!(res);
+    fn p2(&self, data: String) {
+        let ll = data_parser(data);
+        let count = safe_count(ll, is_dampener_safe);
+        dbg!(count);
     }
 }
 
@@ -31,40 +27,101 @@ fn data_parser(raw: String) -> Vec<Vec<i64>> {
         .collect::<Vec<Vec<i64>>>()
 }
 
+type SafetyCheck = fn(&Vec<i64>) -> i64;
+
 
 fn is_safe(row: &Vec<i64>) -> i64 {
-    println!("next row!");
-
     let dif = row[1] - row[0];
     if dif == 0 {
         return 0
     }
     let dir =  (dif) / (dif).abs();
-    dbg!(&dir);
     let mut s = row[0];
-    dbg!(&s);
     for i in 1..row.len() {
-        dbg!(&row[i]);
         let min = (dir * 1) + s;
-        dbg!(&min);
         let max = (dir * 3) + s;
-        dbg!(&max);
         if dir * row[i] < dir * min || dir * row[i] > dir * max {
             return 0
         }
 
         s = row[i];
-        dbg!("s", &s);
     }
 
-    1
+    return 1
 }
 
-fn safe_count(data: Vec<Vec<i64>>) -> i64 {
+
+// fn is_damp_safe(row: &Vec<i64>) -> i64 {
+//     let mut dif = Vec::<i64>::new();
+//     let mut i = 0;
+//     loop {
+//         if i + 1 > row.len() { break; }
+
+//         dif.push(row[i+1] - row[i]);
+//         i += 1;
+//     }
+
+//     for c in dif {
+        
+//     }
+
+//     return 1
+// }
+
+fn is_dampener_safe(row: &Vec<i64>) -> i64 {
+    let mut dampened = false;
+    let mut reverse = false;
+    let mut jump = 0;
+    let mut dif = row[1] - row[0];
+    if dif == 0 && !dampened {
+        dif = row[2] - row[0];
+        dampened = true;
+    }
+
+    if dif == 0 {
+        return 0
+    }
+    let mut dir =  (dif) / (dif).abs();
+    let mut s = row[0];
+    let mut i = 0;
+    loop {
+        i += 1;
+        if i >= row.len() {
+            break;
+        }
+
+        if i == jump {
+            continue;
+        }
+
+        let min = (dir * 1) + s;
+        let max = (dir * 3) + s;
+        if dir * row[i] < dir * min || dir * row[i] > dir * max {
+            jump = i;
+            if !dampened {
+                dampened = true;
+                continue;
+            }
+
+            if !reverse {
+                reverse = true;
+                i = 0;
+                dir *= -1;
+                continue;
+            }
+
+            return 0
+        }
+
+        s = row[i];
+    }
+
+    return 1
+}
+
+fn safe_count(data: Vec<Vec<i64>>, func: SafetyCheck) -> i64 {
     data.iter()
-        .map(|row| is_safe(row))
-        // .collect::<Vec<i64>>()
-        // .iter()
+        .map(|row| func(row))
         .sum()
 }
 
@@ -103,23 +160,23 @@ mod tests {
 1 3 6 7 9
 ";
         let ll = data_parser(raw.to_string());
-        let count = safe_count(ll);
+        let count = safe_count(ll, is_safe);
         dbg!(&count);
         assert_eq!(count, 2)
     }
 
     #[test]
-    fn similarity_test() {
-        let raw = "3   4
-4   3
-2   5
-1   3
-3   9
-3   3
+    fn safe_dampener_count_test() {
+        let raw = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
 ";
-        let _ll = data_parser(raw.to_string());
-        // let m2 = into_map(l2);
-        // let res = similarity(l1, m2);
-        // assert_eq!(res, 31)
+        let ll = data_parser(raw.to_string());
+        let count = safe_count(ll, is_dampener_safe);
+        dbg!(&count);
+        assert_eq!(count, 4)
     }
 }
