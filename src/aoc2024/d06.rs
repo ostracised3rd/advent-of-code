d3e6e409107384097a0064a5e11ab6416007ac66
soheil eivazy <eivazy.soheil@gmail.com>
2024-12-07 16:57:28 +0330

    WIP;

--- src/aoc2024/d06.rs
+++ src/aoc2024/d06.rs
@@ -17,38 +17,77 @@ impl advent_of_rust::Day for AoC {
     }
 }
 
-fn data_parser(raw: String) -> (Vec<Vec<i64>>, Vec<Vec<i64>>, (usize, usize)) {
-    let mut map_list: Vec<Vec<i64>> = Vec::new();
-    let mut walk_list: Vec<Vec<i64>> = Vec::new();
-    let mut start: (usize, usize) = (0, 0); 
 
-    for (i, line) in raw.lines().enumerate() {
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+struct Dir {
+    x: i64,
+    y: i64,
+}
+
+type Matrix = Vec<Vec<i64>>;
+
+const UP: Dir = Dir{x:0, y:-1};
+const RIGHT: Dir = Dir{x:1, y:0};
+const DOWN: Dir = Dir{x:0, y:1};
+const LEFT: Dir = Dir{x:-1, y:0};
+
+const Directions: [Dir; 4] = [
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT,
+];
+
+
+
+struct Actor {
+    location: Point,
+    direction: Dir,
+}
+
+impl Actor {
+    fn step(&mut self) {
+        
+    }
+}
+
+
+fn data_parser(raw: String) -> (Matrix, Matrix, Actor) {
+    let mut map_list: Matrix = Vec::new();
+    let mut walk_list: Matrix = Vec::new();
+    let mut actor: Actor = Actor{location: Point{x:0, y:0}, direction: UP}; 
+
+    for (y, line) in raw.lines().enumerate() {
         map_list.push(Vec::new());
         walk_list.push(Vec::new());
-        for (j, c) in line.chars().enumerate() {
+        for (x, c) in line.chars().enumerate() {
             match c {
                 '.' => {
-                    map_list[i].push(0);
-                    walk_list[i].push(0);
+                    map_list[y].push(0);
+                    walk_list[y].push(0);
                 },
                 '#' => {
-                    map_list[i].push(1);
-                    walk_list[i].push(0);
+                    map_list[y].push(1);
+                    walk_list[y].push(0);
                 },
                 '^' => {
-                    map_list[i].push(0);
-                    walk_list[i].push(1);
-                    start = (i, j);
+                    map_list[y].push(0);
+                    walk_list[y].push(1);
+                    actor.location = Point{x, y};
                 },
                 _ => {
-                    map_list[i].push(0);
-                    walk_list[i].push(0);
+                    map_list[y].push(0);
+                    walk_list[y].push(0);
                 }
             }
         }
     }
 
-    (map_list, walk_list, start)
+    (map_list, walk_list, actor)
 }
 
 mod tests {
@@ -68,8 +107,8 @@ mod tests {
 ......#...";
 
         let (map_list, walk_list, start) = data_parser(raw.to_string());
-        assert_eq!(vec![3,4,2,1,3,3], l1);
-        assert_eq!(vec![4,3,5,3,9,3], l2);
+        // assert_eq!(vec![3,4,2,1,3,3], l1);
+        // assert_eq!(vec![4,3,5,3,9,3], l2);
     }
 
 //     #[test]
